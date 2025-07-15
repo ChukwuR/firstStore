0a94a6ca8bf603c5d12b50a666499b7ef16f5d95
ChukwuR <chukwur919@gmail.com>
2025-07-15 11:19:28 +0100

    Fix casing for TampleteCarousel import

--- myapp/src/components/Home.jsx
+++ myapp/src/components/Home.jsx
@@ -4,7 +4,7 @@ import Service from './Service';
 import HomeShowCase from './HomeShowCase';
 import Preview from './Preview';
 import Carousel from './Carousel';
-import TampleteCarousel from '../components/TampleteCarousel.jsx';
+import TampleteCarousel from './TampleteCarousel';
 import Footer from './Footer';
 
 function Home(){
