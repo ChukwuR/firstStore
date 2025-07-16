bba3d4ae67bfbf57ae6873714d1bf95f660b5972
ChukwuR <chukwur919@gmail.com>
2025-07-16 10:50:29 +0100

    update Footer and footer

--- myapp/src/components/Footer.jsx
+++ myapp/src/components/Footer.jsx
@@ -4,54 +4,44 @@ function Footer(){
 
     return(
         <>
-            <div className='bg-dark' style={{border:'5px solid cyan'}}>
-                <div className="row container">
-                    <div className="col-sm-12 col-md-6 col-lg-3">
-                        <div className="card">
-                            <h4>Contact Info</h4>
-                            <p className='lead' id='cardPTxt'>
-                                About Us <br />
-                                Trade In <br />
-                                How to Pay <br />
-                                Find a Store
-                            </p>
-                        </div>
+            <div className='bg-dark' style={{border:'5px solid cyan', color:'white'}}>
+                <div className="row container footerWrap" >
+                    <div className='col-sm-10 col-md-5 col-lg-2'>
+                        <h4>Contact Info</h4>
+                        <ul className='lead'>
+                            <li>About Us</li>
+                            <li>Trade In</li>
+                            <li>How to Pay</li>
+                            <li>Find a Store</li>
+                        </ul>
                     </div>
 
-                    <div className="col-sm-12 col-md-6 col-lg-3">
-                        <div className="card">
-                            <h4>Help</h4>
-                            <p className='lead' id='cardPTxt'>
-                                Contact Us <br />
-                                FAQ <br />
-                                Warranty Policy <br />
-                                Privacy and Cookie <br /> Policy
-                            </p>
-                        </div>
+                    <div className='col-sm-10 col-md-5 col-lg-2'>
+                        <h4>Help</h4>
+                        <ul className='lead'>
+                            <li>Contact Us</li>
+                            <li>FAQ</li>
+                            <li>Warranty Policy</li>
+                            <li>Privacy and Cookie</li>
+                            <li>Policy</li>
+                        </ul>
                     </div>
 
-                    <div className="col-sm-12 col-md-6 col-lg-3">
-                        <div className="card">
-                            <h4>Info</h4>
-                            <p className='lead' id='cardPTxt'>
-                                My Account <br />
-                                Oder Histroy
-                            </p>
-                        </div>
+                    <div className='col-sm-10 col-md-5 col-lg-2'>
+                        <h4>Info</h4>
+                        <ul className='lead'>
+                            <li>My Account</li>
+                            <li>Older History</li>
+                        </ul>
                     </div>
 
-                    <div className="col-sm-12 col-md-6 col-lg-3">
-                        <div className="card">
-                            <h4>Contact Us</h4>
-                            <p className='lead' id='cardPTxt'>
-                                Call us 24/7
-                                <br />
-                                08020770820
-                                <br />
-                                Gbenga Akinola Street, Ikorodu, <br /> Lagos <br />
-                                <i className='facebook'></i>
-                            </p>
-                        </div>
+                    <div className='col-sm-10 col-md-5 col-lg-2'>
+                        <h4>Contact Us</h4>
+                        <ul className='lead'>
+                            <li>Active 24/7</li>
+                            <li>+2348020770820</li>
+                            <li>Gbenga Akinola Street, Ikorodu, Lagos</li>
+                        </ul>
                     </div>
                 </div>
                 <div className="container d-flex social-icons neon-container">
